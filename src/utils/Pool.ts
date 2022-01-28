b531587d4e2461a6cf373354da18a22794d9c0a5
Sergey Voronkov <sergey@voronkov.me>
2022-01-28 19:24:29 +0500

    Пул обьктов

--- src/utils/Pool.ts
+++ src/utils/Pool.ts
@@ -1,26 +1,29 @@
-export namespace ecs {
-	export interface IPoolObject {
-		init(...args:any[]):void
-		release():void;
+export interface IPoolObject {
+	init(...args: any[]): void
+
+	release(): void;
+}
+
+export abstract class Pool<T extends IPoolObject> {
+	_items: T[] = [];
+
+	push(item: T) {
+		this._items.push(item);
+		item.release();
 	}
 
-	export abstract class Pool<T extends IPoolObject> {
-		_items:T[] = [];
-
-		push(item:T) {
-			this._items.push(item);
-			item.release();
+	create(...args: any[]): T {
+		let item = this._items.pop();
+		if (!item) {
+			item = this._generate();
 		}
-
-		create(...args:any[]): T {
-			let item = this._items.pop();
-			if(!item) {
-				item = this._generate();
-			}
-			item.init(...args);
-			return item;
-		}
-
-		abstract _generate():T
+		item.init(...args);
+		return item;
 	}
+
+	get size() {
+		return this._items.length;
+	}
+
+	abstract _generate(): T
 }
--- tests/pool.ts
+++ tests/pool.ts
@@ -1,6 +1,4 @@
-import {ecs} from '../src/utils/Pool';
-import Pool = ecs.Pool;
-import IPoolObject = ecs.IPoolObject;
+import {Pool,IPoolObject} from '../src/utils/Pool';
 import assert from 'assert';
 
 
@@ -35,24 +33,30 @@ class TestPool extends Pool<TestPoolObject> {
 
 describe('Пул объектов', function () {
 	const pool = new TestPool();
-
 	let object = pool.create(1, 'Object 1');
 	it('Создание', async function () {
+		let object = pool.create(1, 'Object 1');
 		assert(object, 'Не создан');
+		pool.push(object);
 	})
 
 	it('Нужный тип', async function () {
+		let object = pool.create(1, 'Object 1');
 		assert(object instanceof TestPoolObject, 'Не создан');
+		pool.push(object);
 	})
 
 	it('Возврат обьекта в пул', async function () {
+		let object = pool.create(1, 'Object 1');
 		pool.push(object);
 	})
 
 	it('Достаем обратно должен быть тот же обьект но с другими данными', async function () {
 		let clone = Object.assign({}, object);
+		pool.push(object);
 		let object2 = pool.create(2, 'Object 2');
 		assert(object === object2, 'Не тот обьект');
+
 		assert.notStrictEqual(clone, object2, 'Не записались новые данные');
 		assert.deepStrictEqual(Object.assign({}, object2), {id: 2, name:'Object 2'}, "Данные не каректно записались ")
 	})
